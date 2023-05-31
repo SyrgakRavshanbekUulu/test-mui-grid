ffda1aae375e430aa74750420921facb477cead9
SyrgakRavshanbekUulu <syrgakravshanbekuulu@gmail.com>
2023-05-31 18:21:48 +0600

    ignore not found

--- src/shared/utils/app.ts
+++ src/shared/utils/app.ts
@@ -4,6 +4,8 @@ import { clearAll, read } from 'service/storage';
 
 export const checkUrl = ({ pathname }: Location) => {
   const notFound = !hasMatch(pathname);
+  // git hub page not working
+  return '/'
 
   if (notFound) return '/not-found';
 }
